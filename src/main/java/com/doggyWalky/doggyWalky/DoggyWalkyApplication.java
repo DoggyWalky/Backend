954e6a8a18f34f12d5468587b3b50cca6c36a369
xodnjs8287 <115204015+xodnjs8287@users.noreply.github.com>
2024-01-31 18:00:19 +0900

    build

---
+++ src/main/java/com/doggyWalky/doggyWalky/DoggyWalkyApplication.java
@@ -0,0 +1,13 @@
+package com.doggyWalky.doggyWalky;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoggyWalkyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoggyWalkyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/doggyWalky/doggyWalky/DoggyWalkyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.doggyWalky.doggyWalky;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoggyWalkyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
