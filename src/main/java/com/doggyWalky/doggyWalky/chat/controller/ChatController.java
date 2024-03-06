f5c7a1d6c55b94f57e3558a27a6ebcacf8f040ab
kangwook <rlarkddnr1686@naver.com>
2024-03-06 14:44:38 +0900

    Feat: 채팅 나가기, 채팅 안보이게 하는 설정 요청 추가

--- src/main/java/com/doggyWalky/doggyWalky/chat/controller/ChatController.java
+++ src/main/java/com/doggyWalky/doggyWalky/chat/controller/ChatController.java
@@ -1,15 +1,14 @@
 package com.doggyWalky.doggyWalky.chat.controller;
 
-import com.amazonaws.Response;
 import com.doggyWalky.doggyWalky.chat.dto.ChatRoomMessage;
 import com.doggyWalky.doggyWalky.chat.dto.request.ContactRequestDto;
-import com.fasterxml.jackson.databind.ObjectMapper;
+import com.doggyWalky.doggyWalky.chat.dto.request.QuitRequestDto;
+import com.doggyWalky.doggyWalky.chat.dto.request.UnvisibleRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.listener.ChannelTopic;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -30,6 +29,22 @@ public class ChatController {
         Long senderId = Long.parseLong(principal.getName());
 
         redisPubTemplate.convertAndSend(topic.getTopic(), ChatRoomMessage.createChatRoom(senderId,contactDto.getReceiverId(),contactDto.getJobPostId()));
+        return new ResponseEntity(HttpStatus.OK);
+    }
+
+    @PostMapping("/unvisible")
+    public ResponseEntity unvisible(@RequestBody UnvisibleRequestDto unvisibleDto, Principal principal) {
+        Long senderId = Long.parseLong(principal.getName());
+
+        redisPubTemplate.convertAndSend(topic.getTopic(), ChatRoomMessage.unvisibleChatRoom(senderId, unvisibleDto.getReceiverId(), unvisibleDto.getChatRoomId()));
+        return new ResponseEntity(HttpStatus.OK);
+    }
+
+    @PostMapping("/quit")
+    public ResponseEntity quit(@RequestBody QuitRequestDto quitDto, Principal principal) {
+        Long senderId = Long.parseLong(principal.getName());
+
+        redisPubTemplate.convertAndSend(topic.getTopic(), ChatRoomMessage.quitChatRoom(senderId, quitDto.getReceiverId(), quitDto.getChatRoomId()));
         return new ResponseEntity(HttpStatus.OK);
     }
 }
